2902b8f8690a1eee6300c5b41219020884c659ea
RafaelAlfonso GilVelarde <20192303@aloe.ulima.edu.pe>
2021-11-25 23:41:48 -0500

    actualizado

--- index.js
+++ index.js
@@ -26,6 +26,48 @@ app.get('/', (req, res) => {
 })
 
 ////////////////////////////////////////////////////////////////////////////////////////
+app.get('/cliente/new',(req,res)=>{
+
+	res.render('cliente_new')
+})
+app.post('/cliente/new',async(req,res)=>{
+	const ClienteNom=req.body.cliente_nombre
+	const ClienteApp=req.body.cliente_apellido
+	const ClienteDNI=req.body.cliente_DNI
+	const ClienteFoto=req.body.cliente_foto
+	const ClienteCorreo=req.body.cliente_correo
+	const ClienteCont=req.body.cliente_contraseña
+	const ClienteTelf=req.body.cliente_telefono
+	const ClienteDep=req.body.cliente_departamento
+	const ClienteProv=req.body.cliente_provincia
+	const ClienteDist=req.body.cliente_distrito
+	const ClienteDir=req.body.cliente_direccion
+	let ClientePEP=null
+	if(req.body.cliente_PEP==true){
+		ClientePEP=1
+	}
+	else{
+		ClientePEP=0
+	}
+
+			await db.Cliente.create({
+				nombre: ClienteNom,
+				apellidos: ClienteApp,
+				dni: ClienteDNI,
+				foto: ClienteFoto,
+				correo:ClienteCorreo,
+				contraseña: ClienteCont,
+				telefono: ClienteTelf,
+				departamento: ClienteDep,
+				provincia: ClienteProv,
+				distrito: ClienteDist,
+				direccion: ClienteDir,
+				esPEP: ClientePEP
+			})
+			res.redirect('/Espera')
+	
+})
+////////////////////////////////////////////////////////////////////////////////////////
 
 app.listen(PORT, () => {
 	console.log(`Se ha inicializado el servidor en el puerto ${PORT}`)
