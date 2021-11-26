e502e7b61b57713dd2f040112dac0e43dc13fac3
Jesus Campos <jesus.campos123@icloud.com>
2021-11-26 00:27:56 -0500

    Configuración de rutas corregida

--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const db = require('../dao/config')
+const db = require('../dao/models')
+
+router.use(require('./partidas_editar'))
+router.use(require('./partidas_nuevo'))
+router.use(require('./partidas'))
 
 router.get('/', (req, res) => {
 	res.send('HOLA MUNDO')
