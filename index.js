33dec16330f44f377ed225d1d773c148765c4db9
Bruno Herman Wong Saldias <bwongsaldias@hotmail.com>
2021-11-28 13:01:27 -0500

    avance

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const session = require('express-session')
 const bodyParser = require('body-parser')
 const db = require('./dao/models')
+const ejs=require('ejs')
+const path= require('path')
 
 const PORT = 5000
 const app = express()
@@ -18,12 +20,47 @@ app.use(bodyParser.urlencoded({
 }))
 
 app.set('view engine', 'ejs')
+//el tradicional path join
+app.use(express.static('assets'))
 
 ////////////////////////////////////////////////////////////////////////////////////////
-
+//se creo el acceso al login
+//ya se puso script para corroborar contra y username , pero falta  compararlo a la db 
 app.get('/', (req, res) => {
 	res.render('login')
 })
+
+//aqui el post funciona pero con scripts , falta la db
+app.post('/login',(req,res)=>{
+	res.render('admin_menu')
+	
+})
+
+//redirecciona a menu cuando entra con el login
+app.get('/menu',(req,res)=>{
+	res.render('menu')
+})
+//cada que le das click a banner te redireccion a una pagina
+app.get('/banners',(req,res)=>{
+	res.render('banners')
+})
+//cada que le das click a categoria te redireccion a una pagina
+
+app.get('/categorias',(req,res)=>{
+	res.render('categorias')
+})
+//cada que le das click a juegos te redireccion a una pagina
+app.get('/juegos',(req,res)=>{
+	res.render('juegos')
+})
+//cada que le das click a partidas te redireccion a una pagina
+app.get('/partidas',(req,res)=>{
+	res.render('partidas')
+})
+
+
+
+
 
 ////////////////////////////////////////////////////////////////////////////////////////
 
