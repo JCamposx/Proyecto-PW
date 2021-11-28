e701b31ee16a015c6e5b6f78716dccef4ffe90c5
Jenis Arnold Alvarado Gonzales <arnoldag98@gmail.com>
2021-11-28 16:57:58 -0500

    avance en filter

--- routes/routes.js
+++ routes/routes.js
@@ -6,11 +6,14 @@ router.use(require('./juegos'))
 router.use(require('./juegos_nuevo'))
 router.use(require('./juegos_modificar'))
 router.use(require('./juegos_eliminar'))
+router.use(require('./juegos_filtrar'))
+
 router.use(require('./banners'))
 router.use(require('./banners_nuevo'))
 router.use(require('./banners_modificar'))
 router.use(require('./banners_eliminar'))
 
+router.use(require('./reglas'))
 
 router.get('/', (req, res) => {
 	res.send('HOLA MUNDO')
