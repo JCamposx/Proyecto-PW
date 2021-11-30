285c9d78ef5261b8aa352c469146c7bf28016518
Bruno Herman Wong Saldias <bwongsaldias@hotmail.com>
2021-11-30 16:48:11 -0500

    sesion clientes

--- routes/cliente.js
+++ routes/cliente.js
@@ -2,11 +2,11 @@ const express = require('express')
 const router = express.Router()
 const db = require('../dao/models')
 
-app.get('/cliente',(req,res)=>{
+//app.get('/cliente',(req,res)=>{
 
-	res.render('cliente_historial',{
-		partidas: partidas
-	})
-})
+//	res.render('cliente_historial',{
+//		partidas: partidas
+//	})
+//})
 
 module.exports = router
--- routes/menu.js
+++ routes/menu.js
@@ -2,13 +2,12 @@ const express = require('express')
 const router = express.Router()
 const db = require('../dao/models')
 
-app.get('/menu',(req,res)=>{
-	res.render('menu')
-})
+//app.get('/menu',(req,res)=>{
+//	res.render('menu')
+//})
 
-app.get('/menucliente',async(req,res)=>{
-	const juegos = await db.Juego.findAll()
-	res.render('cliente_menu',{juegos:juegos})
-})
-
+//app.get('/menucliente',async(req,res)=>{
+//	const juegos = await db.Juego.findAll()
+//	res.render('cliente_menu',{juegos:juegos})
+//})
 module.exports = router
