bc3d24a1f79919ec9caaeda13093a7c5027dd5de
Bruno Herman Wong Saldias <bwongsaldias@hotmail.com>
2021-12-01 12:58:06 -0500

    Data chance

--- dao/seeders/20211201171839-data.js
+++ dao/seeders/20211201171839-data.js
@@ -15,35 +15,40 @@ module.exports = {
     */
     //Equipo
     await queryInterface.bulkInsert('Equipo',[
-      {nombre : "Alianza Lima"},
-      {nombre : "Cristal"}
+      {nombre : "AlianzaLima", createdAt : new Date(), updatedAt : new Date()},
+      {nombre : "Cristal", createdAt : new Date(), updatedAt : new Date()}
     ])
 
     await queryInterface.bulkInsert('Partida',[
       {
-        fecha : new Date().getTime(),
-        hora_inicio : "12:40",
-        duracion : 60,
-        factor_local : 1.2,
-        factor_visita : 2.4,
-        factor_empate : 0.6,
-        resultado : 1,
-        estado : 2,
-        id_local : 1,
-        id_visita : 2
+        fecha: '12/03/21',
+		  	hora_inicio: '15:30',
+			  duracion: 90,
+			  factor_local: 1.3,
+			  factor_visita: 21.3,
+			  factor_empate: 2.5,
+			  resultado: 0,
+			  estado: 0,
+			  id_local: 1,
+			  id_visita: 3,
+			  createdAt: new Date(),
+			  updatedAt: new Date(),
+			  id_juego: 1
       },
       {
-        
-        fecha : new Date().getTime(),
-        hora_inicio : "14:40",
-        duracion : 90,
-        factor_local : 1.1,
-        factor_visita : 2.6,
-        factor_empate : 0.7,
-        resultado : 2,
-        estado : 3,
-        id_local : 3,
-        id_visita : 4
+        fecha: '14/04/21',
+			  hora_inicio: '15:00',
+			  duracion: 90,
+			  factor_local: 1.2,
+			  factor_visita: 5.1,
+			  factor_empate: 2.3,
+			  resultado: 0,
+			  estado: 0,
+			  id_local: 10,
+			  id_visita: 11,
+			  createdAt: new Date(),
+			  updatedAt: new Date(),
+			  id_juego: 3
       }
     ])
 
@@ -53,14 +58,16 @@ module.exports = {
         id_cliente : 1,
         ganancia : 20,
         decision : 2,
-        dinero_apostado : 50
+        dinero_apostado : 50,
+        createdAt : new Date(), updatedAt : new Date()
     },
       {
         id_partida : 2,
         id_cliente : 2,
         ganancia : 24,
         decision : 1,
-        dinero_apostado : 100
+        dinero_apostado : 100,
+        createdAt : new Date(), updatedAt : new Date()
       }
     ])
   },
