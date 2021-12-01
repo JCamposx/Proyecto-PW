9d8ba74b9099d8753ae7a675fa2a634326465e24
Bruno Herman Wong Saldias <bwongsaldias@hotmail.com>
2021-12-01 12:34:20 -0500

    Actualizar data

--- dao/seeders/20211201171839-data.js
+++ dao/seeders/20211201171839-data.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const { TIME } = require("sequelize");
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     /**
@@ -11,7 +13,58 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
+    //Equipo
+    await queryInterface.bulkInsert('Equipo',[
+      {nombre : "Alianza Lima"},
+      {nombre : "Cristal"}
+    ])
+
+    await queryInterface.bulkInsert('Partida',[
+      {
+        fecha : new Date().getTime(),
+        hora_inicio : "12:40",
+        duracion : 60,
+        factor_local : 1.2,
+        factor_visita : 2.4,
+        factor_empate : 0.6,
+        resultado : 1,
+        estado : 2,
+        id_local : 1,
+        id_visita : 2
+      },
+      {
+        
+        fecha : new Date().getTime(),
+        hora_inicio : "14:40",
+        duracion : 90,
+        factor_local : 1.1,
+        factor_visita : 2.6,
+        factor_empate : 0.7,
+        resultado : 2,
+        estado : 3,
+        id_local : 3,
+        id_visita : 4
+      }
+    ])
+
+    await queryInterface.bulkInsert('Apuesta',[
+      {
+        id_partida : 1,
+        id_cliente : 1,
+        ganancia : 20,
+        decision : 2,
+        dinero_apostado : 50
+    },
+      {
+        id_partida : 2,
+        id_cliente : 2,
+        ganancia : 24,
+        decision : 1,
+        dinero_apostado : 100
+      }
+    ])
   },
+
 
   down: async (queryInterface, Sequelize) => {
     /**
@@ -20,5 +73,8 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+    await queryInterface.bulkInsert('Equipo',null, {})
+    await queryInterface.bulkInsert('Partida',null, {})
+    await queryInterface.bulkInsert('Apuesta',null, {})
   }
 };
